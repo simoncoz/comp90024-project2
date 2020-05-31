db10fc866aa6a4361d4c261aed0c76a96d05d83a
Simon Cozens <Simon.Cozens@team.telstra.com>
2020-05-31 12:45:39 +1000

    uses env vars for couchdb

--- Harvester/main.py
+++ Harvester/main.py
@@ -3,8 +3,10 @@ import sys
 import os
 import couchdb
 
+
 from tweepy import OAuthHandler
- 
+
+# Twitter auth - #TODO change to env vars 
 consumer_key = 'SCUhOySMXyXfwi2G4WcnzF18i'
 consumer_secret = 'n5oIIuFnbDjkzZ4YL3b6xuQ57cEVCjPJajVmMa5842q373mgqi'
 access_token = '1256415422137556993-jxEOTaE1r2p7x5HlRitpIYQRRjSRoz'
@@ -16,14 +18,18 @@ auth.set_access_token(access_token, access_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True,
 				   wait_on_rate_limit_notify=True)
 
-couch = couchdb.Server('http://admin:admin@127.0.0.1:5984/')
+# CouchDB auth
+env_user = os.environ.get('user')
+env_pass = os.environ.get('pass')
+print(f"http://{env_user}:{env_pass}@127.0.0.1:5984/")
+couch = couchdb.Server(f"http://{env_user}:{env_pass}@127.0.0.1:5984/")
 db = couch['tweetsdb']
 
 places = api.geo_search(query="Australia", granularity="country")
 place_id = places[0].id
 
 maxTweets = 50000 # Some arbitrary large number
-fName = 'tweets7.txt' # We'll store the tweets in a text file.
+# fName = 'tweets7.txt' # We'll store the tweets in a text file.
 
 max_id = -1
 tweetCount = 0
@@ -38,7 +44,8 @@ while tweetCount < maxTweets:
       for tweet in tweets:
         place = tweet.place.name if tweet.place else "Undefined place"
         doc = {'id': str(tweetCount+1), 'text': tweet.text, 'location': place, 'lang': tweet.lang}
-        doc = {'_id': '1', 'text': 't', 'location': 'place', 'lang': 'tweet'}
+        #doc = {'_id': '1', 'text': 't', 'location': 'place', 'lang': 'tweet'}
+        print (doc)
         print(doc['id'])
         db.save(doc)
         #f.write(tweet.text + "\n" + place)
